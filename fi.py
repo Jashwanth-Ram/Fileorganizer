a06194a39971e3f3ddc43b87603d70cbd39e82db
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2023-05-21 16:17:00 +0530

    Add files via upload

---
+++ fi.py
@@ -0,0 +1,34 @@
+import shutil
+import os
+
+def file_organizer():
+    path = input("Enter path: ")
+    file_list = os.listdir(path)
+
+    for file in file_list:
+        file_name, extension = os.path.splitext(file) # retrieving filename and extension from each file
+        extension = extension[1:] # getting rid of dot operator
+
+        if os.path.exists(path+"/"+extension):
+            shutil.move(path+"/"+file, path+"/"+extension+"/"+file)
+        else:
+            os.makedirs(path+"/"+extension)
+            shutil.move(path+"/"+file, path+"/"+extension+"/"+file)
+
+            
+file_organizer();
+
+
+
+        
+
+
+
+
+
+
+    
+    
+
+    
+
